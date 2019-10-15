910d07d3e2afaebf7ff185a0a5334f6ec554a95b
Jandy <reverdy@protonmail.com>
2019-10-15 14:48:13 -0700

    deleted connection file

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,8 @@ var authRoutes = require('./authorization');
 var apiRoutes = require('./api-routes')
 var htmlRoutes = require('./html-routes')
 
-<<<<<<< HEAD
-=======
 router.use('/', htmlRoutes)
 router.use('/api', apiRoutes)
->>>>>>> b7b572fd98e98e31e6c16b434d90e4fb0872ecfc
 router.use('/auth', authRoutes)
 
 module.exports = router;
