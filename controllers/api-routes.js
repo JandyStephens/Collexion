29a65cc76460218f834e8e750ab26ca725c34585
Jandy <reverdy@protonmail.com>
2019-10-16 10:58:58 -0700

    sign in and log in redirects to myCollexions

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -2,7 +2,7 @@
 //==========================================================
 var express = require("express");
 var router = express.Router();
-var db = require("./models");
+var db = require("../models");
 
 
 
